710473657a103ea023bd6a0dbcaff219df66c612
suxideveloper <suxidev@gmail.com>
2025-10-14 10:24:33 +0500

    Core

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ x, y = odam.ism, odam.yosh
 salom = odam.greet()
 
 
-print(x, y)
-print(salom)
+# print(x, y)
+# print(salom)
 
+class Dog:
+    species = "Canine"  # Class attribute shared by all instances of the class.
+    def __init__(self, name, age): # __init__ method: Initializes the name and age attributes when a new object is created
+        self.nomi = name  # Instance attribute
+        self.yoshi = age  # Instance attribute
+
+it = Dog("Apch", "wcbe")
+print(it.nomi)
+print(Dog.species)
+
+class Car:
+    def __init__(self, model, price):
+        self.rusum = model
+        self.narxi = price
+
+
+Audi = Car(123,234) 
+
+modeli = Audi.rusum
+narxi = Audi.narxi
+
+
+
+# print(modeli, narxi)
